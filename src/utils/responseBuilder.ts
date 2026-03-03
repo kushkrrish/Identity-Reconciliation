798195fc03e5456f8fb87a4a6c37aa7ed46cb626
kushagra kapoor <kushagra16.kapoor@gmail.com>
2026-03-03 22:49:36 +0530

    created response builder

--- src/utils/responseBuilder.ts
+++ src/utils/responseBuilder.ts
@@ -1,3 +1,23 @@
 export default function buildResponse(primary:any,secondaries:any[]){
+    try {
+        const emails=[
+            primary.email,...secondaries.map(s=>s.email)
+        ].filter(Boolean);
 
+        const phones=[
+            primary.phoneNumber,
+            ...secondaries.map(s=>s.phoneNumber)
+        ].filter(Boolean);
+
+        return {
+            contact:{
+                primaryContactId:primary.id,
+                emails:[...new Set(emails)],
+                phoneNumbers:[...new Set(phones)],
+                secondaryContacts:secondaries.map(s=>s.id)
+            }
+        }
+    } catch (error) {
+        throw new Error(String(error));
+    }
 }
